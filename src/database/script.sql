690b03eadea196b00b6fa385cf854b5b52c23e02
AdamanLino <adaman.santos@sptech.school>
2025-05-24 16:41:56 -0300

    mudancas no script e modelagem do bd

--- src/database/script.sql
+++ src/database/script.sql
@@ -1,5 +1,6 @@
 -- criação da database
 create database sonicfansite;
+
 use sonicfansite;
 
 -- criação das tabelas
@@ -28,8 +29,8 @@ create table usuario (
 create table topico (
 	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     email varchar(80),
-    assunto varchar(100),
-	comentario varchar (280),
+    assunto varchar(100) NOT NULL,
+	comentario varchar (280) NOT NULL,
     dtCriacao datetime not null default(now()),
     fkusuario INT NOT NULL,
     FOREIGN KEY (fkusuario) references usuario(id)
@@ -38,9 +39,8 @@ create table topico (
 create table mensagem (
 	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     email varchar(80),
-    assunto varchar(100),
-	comentario varchar (280),
-    dtCriacao datetime,
+	comentario varchar (280) NOT NULL,
+    dtCriacao datetime not null default(now()),
     fktopico INT NOT NULL,
     fkusuario INT NOT NULL,
     FOREIGN KEY (fktopico) references topico(id),
@@ -49,11 +49,24 @@ create table mensagem (
 
 create table enquete (
 	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    pergunta varchar(280),
-    alternativas varchar(280),
-    dtCriacao datetime,
-    fkusuario INT NOT NULL,
-    FOREIGN KEY (fkusuario) references usuario(id)
+    pergunta varchar(280) NOT NULL,
+    dtCriacao datetime not null default(now())
+);
+
+create table alternativa (
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  texto varchar(280),
+  fkenquete int,
+  foreign key (fkenquete) references enquete(id)
+);
+
+create table usuarioEnquete (
+	fkusuario int NOT NULL,
+    fkalternativa int NOT NULL,
+    primary key(fkusuario, fkalternativa),
+    foreign key (fkusuario) references usuario(id),
+    foreign key (fkalternativa) references alternativa(id),
+    unique (fkusuario, fkalternativa) -- somente um usuario pode votar na enquete
 );
 
 -- inserts
@@ -62,7 +75,8 @@ insert into usuario (nome, email, senha, dtCriacao, fkpermissao)
 		  ('TailsFan', 'tailsfan@outlook.com', 'senhaMariana123', now(), 2), -- moderador
 		  ('Gabriel Echidna', 'gabriel@gmail.com', 'senhaGabrie123', now(), 3), -- membro
 		  ('Ana Rose', 'ana@protonmail.me', 'senhaAna123', now(), 3), -- membro
-		  ('Rafael ShadowX', 'rafael@gmail.com', 'senhaRafael23', now(), 3); -- membro
+		  ('Rafael ShadowX', 'rafael@gmail.com', 'senhaRafael23', now(), 3), -- membro
+          ('SonicN1Fan', 'sonic@gmail.com', '123', now(), 3); -- membro
 
 insert into topico (assunto, comentario, dtCriacao, fkusuario) 
 	values ('Melhor fase do Sonic?', 'Na minha opinião, Green Hill Zone é a melhor fase até hoje!', now(), 1),
@@ -75,10 +89,29 @@ insert into mensagem (comentario, dtCriacao, fktopico, fkusuario)
 		  ('Eu prefiro o Hyper Sonic, mas ele não é canônico e só aparece no Sonic 3 & Knuckles.', now(), 3, 5),
 		  ('Super Sonic é mais clássico e fácil de conseguir.', now(), 3, 1);
 
-insert into enquete (pergunta, alternativas, dtCriacao, fkusuario) 
-	values('Qual a sua fase favorita do Sonic?', 'Green Hill Zone; Chemical Plant Zone; Sky Sanctuary; Stardust Speedway', now(), 1),
-		  ('Qual seu personagem preferido da franquia Sonic?', 'Sonic; Tails; Knuckles; Shadow; Amy; Dr. Eggman', now(), 2),
-		  ('Qual a sua super forma do Sonic favorita?', 'Super Sonic; Hyper Sonic; Werehog', now(), 3);
+insert into enquete (pergunta) 
+	values ('Qual a sua fase favorita do Sonic?'),
+		   ('Qual seu personagem preferido da franquia Sonic?'),
+		   ('Qual a sua super forma do Sonic favorita?');
+
+insert into alternativa (texto, fkenquete) 
+	values ('Green Hill Zone', 1),
+		   ('Chemical Plant Zone', 1),
+		   ('Sky Sanctuary', 1),
+		   ('Stardust Speedway', 1);
+
+insert into alternativa (texto, fkenquete) 
+	values ('Sonic', 2),
+		   ('Tails', 2),
+		   ('Knuckles', 2),
+		   ('Shadow', 2),
+		   ('Amy', 2),
+		   ('Dr. Eggman', 2);
+
+insert into alternativa (texto, fkenquete) 
+	values ('Super Sonic', 3),
+		   ('Hyper Sonic', 3),
+		   ('Werehog', 3);
 
 -- selects
 -- mostra os dados dos usuários e seus cargos
@@ -103,17 +136,32 @@ on top.id = men.fktopico;
 select * from enquete;
 
 -- selects da dashboard
+-- gráfico de total de postagens por tópico
 SELECT t.assunto, count(m.comentario) AS total_mensagens
 FROM topico t
 LEFT JOIN mensagem m ON t.id = m.fktopico
-GROUP BY t.id, t.assunto;
+GROUP BY t.id, t.assunto
+having total_mensagens <= 5;
 
+-- gráfico de total de mensagens por usuário
+SELECT u.nome,
+	count(m.comentario) as total_comentarios
+from usuario u
+inner join mensagem m on u.id = m.fkusuario
+group by u.nome
+order by total_comentarios desc;
+
+-- grafico de novas postagens
+select t.assunto, t.dtCriacao
+from topico t;
 
 -- TESTES
+/*
 select * from topico;
 select * from mensagem;
 insert into topico(assunto, comentario, fkusuario)
 	values ('Assuntos variados', 'Comentario top', 4);
 
+select * from usuario;
 insert into mensagem (comentario, dtCriacao, fktopico, fkusuario) 
-	values('TESTE', now(), 24, 4);
+	values('TESTE DE RESPOSTA', now(), 4, 5);
