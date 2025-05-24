cb17f1f1ab740ae9528e90390b96d7def9aed7a4
AdamanLino <adaman.santos@sptech.school>
2025-05-24 15:21:09 -0300

    mudanças na modelagem

---
+++ src/models/novasPostagensModel.js
@@ -0,0 +1,19 @@
+var database = require("../database/config");
+
+function buscarUltimasMensagens() {
+
+    var instrucaoSql = `
+    SELECT u.nome,
+        count(m.comentario) as total_comentarios
+    from usuario u
+    inner join mensagem m on u.id = m.fkusuario
+    group by u.nome
+    order by total_comentarios desc;`
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+module.exports = {
+    buscarUltimasMensagens
+};
